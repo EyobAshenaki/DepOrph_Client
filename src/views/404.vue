08cf6b3939eb17b0965e0b02463b6f88b3601436
Adem Mohammed <adem164416@gmail.com>
2021-07-27 15:50:28 +0300

    Added chages to     AppNavBar.vue         + Implemented logout logic and functionality     router/index.js, 404.vue, 403.vue         + Implemented route guards         + Added NotFound and Forbidden routes     Login.vue         + Asserted visual consistency with the rest of the app         + Added 'Signup Request' and 'Reset Password' forms with their functionality     Head_v2.vue         + Added account creation info(email and autogenerated password) dialog         + Added Main page for Head as a list of Account Maintanence Requests with the ability to accept or reject the reqests

---
+++ src/views/404.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container grid-list-xs>
+    <v-card>
+      <v-card-title primary-title>
+        <h1 class="error--text">Error 404 - Page Not Found!</h1>
+      </v-card-title>
+      <v-divider></v-divider>
+      <v-card-actions>
+        <v-btn color="info" @click="$router.push('/')"
+          ><v-icon class="mr-2">mdi-arrow-left</v-icon>Back to safety</v-btn
+        >
+      </v-card-actions>
+    </v-card>
+  </v-container>
+</template>
